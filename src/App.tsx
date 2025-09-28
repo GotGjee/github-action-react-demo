1afbd7fd642cf1b48563760d9299301e10bec5d1
GotGjee <6305001916@rumail.ru.ac.th>
2025-09-28 15:30:23 +0700

    noti wt discord

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Github Action Workshop Deploy Docker Image</h1>
+      <h1>Github Action Workshop Deployed via Watch Tower</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
